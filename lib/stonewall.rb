1d8169ee69b736681c540fc4cec588a1c14d6f70
bokmann <dbock@codesherpas.com>
2010-03-20 13:40:35 -0400

    This commit functions in my test app.  I have a refactoring to do, then I'll start the painful process of moving my test app internal to the gem, just like StonePath

--- lib/stonewall.rb
+++ lib/stonewall.rb
@@ -2,23 +2,20 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module StoneWall
-  # main hook into the framework.  From here, this should simply have methods that cause other includes to happen.
   def self.included(base)
 
     base.instance_eval do
       
       def stonewall()
         require File.expand_path(File.dirname(__FILE__)) + "/stonewall/controller.rb"
-        require File.expand_path(File.dirname(__FILE__)) + "/stonewall/state.rb"
-        require File.expand_path(File.dirname(__FILE__)) + "/stonewall/role.rb"
-        cattr_accessor :acl
-        self.acl = StoneWall::Controller.new(self)
-        yield self.acl
+        cattr_accessor :stonewall
+        self.stonewall = StoneWall::Controller.new(self)
+        yield self.stonewall
       end
       
       def self.define_attribute_methods_with_hook
         define_attribute_methods_without_hook
-        acl.fix_aliases if defined? self.acl
+        stonewall.fix_aliases if defined? self.stonewall
       end
       
       class << self
@@ -26,6 +23,21 @@ module StoneWall
         alias_method "define_attribute_methods", "define_attribute_methods_with_hook"
       end
     end
-    
+  end
+ 
+  module UserExtensions
+      def may_send?(object, method)
+        object.class.stonewall.allowed?(object, self, method)
+      end
+  
+      def method_missing_with_stonewall(symb, *args)
+        method_name = symb.to_s
+        if method_name =~ /^may_(.+?)[\!\?]$/
+          args.first.class.stonewall.actions[$1.to_sym].call(args.first, self)
+        else
+          method_missing_without_stonewall(symb, *args)
+        end
+      end
+      alias_method_chain :method_missing, :stonewall
   end
 end
--- lib/stonewall/controller.rb
+++ lib/stonewall/controller.rb
@@ -1,85 +1,137 @@
 module StoneWall
-    class Controller
-
-      attr_reader :guarded_class_methods
-      attr_reader :guarded_instance_methods
-      attr_reader :states
-      attr_reader :guarded_class
-      attr_reader :method_groups
-
-
-
-      def initialize(guarded_class)
-        @guarded_class = guarded_class
-        @guarded_methods = Array.new
-        @states = Hash.new
-        @method_groups = Hash.new
-        @aliases = Array.new
-      end
-
-      def guard_method(method)
-        @guarded_methods << method
-        checked_method = checked_method_name(method)
-        unchecked_method = unchecked_method_name(method)
-
-        guarded_class.send(:define_method, checked_method) do |*args|
-          if acl.allowed?(self, User.current, method)
-            self.send(unchecked_method, *args)
-          else
-            raise "Access Violation"
-          end
-        end
-
-        # Save our needed aliases until later
-        @aliases << [unchecked_method, method]
-        @aliases << [method, checked_method]
-      end
-
-      # fix up our saved aliases
-      def fix_aliases
-        @aliases.each do |src, target|
-          guarded_class.send(:alias_method, src, target)
-        end
-      end
-
-      def method_group(name, methods)
-        @method_groups[name] = methods
-      end
-
-      def for_state(state_name)
-        @states[state_name] = State.new(state_name, self) if @states[state_name].nil?
-        yield @states[state_name]
-      end
-
-      def allowed?(guarded_object, user, method)
-        return true if (guarded_object.nil? || user.nil? || method.nil?)
-        state = guarded_object.aasm_state
-        roles = user.respond_to?(:roles) ? user.roles : [user.role]
-        roles.each { |role| break [true] if states[state].roles[role].allowed?(guarded_object, method) }.uniq[0]
-      end
-
-      private
-
-      # TODO
-      # these two methods are ugly - we need to dry up the repetition and make them work for ? methods.    
-      def checked_method_name(method)
-        method_name = method.to_s
-        if method_name.include?("=")
-          method_name.gsub!("=","")
-          return "#{method_name}_with_check="
-        else
-          return "#{method_name}_with_check"
-        end
-      end
-
-      def unchecked_method_name(method)
-        method_name = method.to_s
-        if method_name.include?("=")
-          method_name.gsub!("=","")
-          return "#{method_name}_without_check="
-        else
-          return "#{method_name}_without_check"
-        end
+  
+  # used internally to parse the dsl.  This class is largely irrelevant in understanding
+  # how to use this framework.  In short, it is the intermediary that helps parse the
+  # dsl into a matrix of hashes.
+  class Parser
+    def initialize(parent, role, variant = :all)
+      @parent, @role, @variant = parent, role, variant
+    end
+    
+    #DSL Term
+    def allowed_method(method)
+      @parent.matrix[@role] ||= Hash.new
+      @parent.matrix[@role][@variant] ||= Hash.new
+      @parent.matrix[@role][@variant][method] ||= Hash.new
+      @parent.matrix[@role][@variant][method] = true
+    end
+    
+    #DSL Term
+    def allowed_methods(method_names)
+      @parent.method_groups[method_names].each do |m|
+        allowed_method m
       end
     end
+    
+    #DSL Term
+    def variant(variant_name)
+      yield Parser.new(@parent, @role, variant_name)
+    end  
+  end
+  
+  
+  # Every class guarded by Stonewall has a Controller.
+  # I eventually want to rename this to be AccessController.
+  #
+  # On Objects of the guarded class, the accessors you declare as 'guard'ed
+  # are wrapped with an alias_method_chain (look it up - you need to know that
+  # to remove the magic of this framework).  The wrapped methods call 'allowed?'
+  # on the AccessController for its class to determine if the permissions
+  # allow that method to be executed.
+  #
+  # TODO:  This class currently has two reposibilities - parsing the DSL, and
+  # working as the AccessController for each class on the running system.  I
+  # think the architecture would be cleaner if that were split into two classes.
+  class Controller
+    attr_reader :guarded_class
+    attr_reader :method_groups
+    attr_reader :actions
+    attr_reader :variant_field
+    attr_accessor :matrix
+    
+    def initialize(guarded_class)
+      @guarded_class = guarded_class
+      @guarded_methods = Array.new
+      @method_groups = Hash.new
+      @actions = Hash.new
+      @aliases = Array.new
+      @matrix = Hash.new
+    end
+
+    # DSL Term
+    def method_group(name, methods)
+      @method_groups[name] = methods
+    end
+
+    # DSL Term
+    def varies_on(state_name)
+      if @variant_field.nil?
+        @variant_field = state_name
+      else
+        raise "no redefinition of variant field"
+      end
+    end
+
+    # DSL Term
+    def role(role_name)
+      yield Parser.new(self, role_name)
+    end
+
+    # DSL Term
+    def action(action_name, &guard)
+      @actions[action_name] = guard 
+    end
+
+    # DSL Term
+    def guard(method)
+      @guarded_methods << method
+      checked_method = synthesized_method_name(method, "with")
+      unchecked_method = synthesized_method_name(method, "without")
+      
+      # Meta to define the checked wrapper method.
+      guarded_class.send(:define_method, checked_method) do |*args|
+        if stonewall.allowed?(self, User.current, method)
+          self.send(unchecked_method, *args)
+        else
+          raise "Access Violation"
+        end
+      end
+
+      # # Save our needed aliases until later
+      @aliases << [unchecked_method, method]
+      @aliases << [method, checked_method]
+    end
+
+    def allowed?(guarded_object, user, method)
+      return true if (guarded_object.nil? || user.nil? || method.nil?)
+
+      variant = variant_field ? guarded_object.send(variant_field) : "all"
+      return true if variant.nil?
+      variant = variant.to_sym
+      
+      roles = (user.respond_to?(:roles) ? user.roles : [user.role]).collect{ |x| x.to_sym}
+ 
+      roles.detect { |r| (matrix[r] && matrix[r][variant] && (matrix[r][variant][method] == true)) } || false
+    end
+    
+    # fix up our saved aliases
+    def fix_aliases
+      @aliases.each do |src, target|
+        guarded_class.send(:alias_method, src, target) # should we convert this to alias_method_chain?
+      end
+    end
+          
+    private
+
+    # given a guarded method name, these two methods synthesize the checked and unchecked method names    
+    def synthesized_method_name(method, s)
+      method_name = method.to_s
+      if method_name.include?("=")
+        method_name.gsub!("=","")
+        return "#{method_name}_#{s}_check="
+      else
+        return "#{method_name}_#{s}_check"
+      end
+    end
+  end
 end
