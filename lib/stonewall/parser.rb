7f443d6b48775efef2d62a4bafc5ce81c74364ce
bokmann <dbock@codesherpas.com>
2010-03-21 12:13:25 -0400

    following a suggestion that I should take arrays, and that the singular and plural method names should do the same thing

--- lib/stonewall/parser.rb
+++ lib/stonewall/parser.rb
@@ -5,16 +5,26 @@ module StoneWall
       @method_groups = Hash.new
     end
 
-    def allowed_method(method)
-      @parent.stonewall.add_grant(@role, @variant, method)
+    def allowed_method(*methods)
+      methods.each { |m| @parent.stonewall.add_grant(@role, @variant, m)}
     end
 
-    def allowed_methods(method_names)
-      @parent.stonewall.method_groups[method_names].each do |m|
-        allowed_method m
+    def allowed_methods(*methods)
+      allowed_method(*methods)
+    end
+    
+    def allowed_method_group(*group_names)
+      group_names.each do |group_name|
+        @parent.stonewall.method_groups[group_name].each do |m|
+          allowed_method m
+        end
       end
     end
 
+    def allowed_method_groups(*group_names)
+      allowed_method_group(group_names)
+    end
+    
     def method_group(name, methods)
       @parent.stonewall.method_groups[name] = methods
     end
@@ -35,25 +45,27 @@ module StoneWall
       yield Parser.new(@parent, @role, variant_name)
     end
 
-    def guard(method)
-      @parent.stonewall.guarded_methods << method
-      aliased_target, punctuation = method.to_s.sub(/([?!=])$/, ''), $1
-      checked_method = "#{aliased_target}_with_stonewall#{punctuation}"
-      unchecked_method = "#{aliased_target}_without_stonewall#{punctuation}"    
-      # --------------
-      # This method is defined on the guarded class, so it is callable on
-      # objects of that class.  This is 1/3rd of the magic of this gem-
-      # if you declare 'schpoo' a guarded method, we generate this
-      # 'schpoo_with_stonewall' method. Elsewhere, we use alias_method_chain
-      # to wrap your original 'schpoo' method.
-      @parent.send(:define_method, checked_method) do |*args|
-        if stonewall.allowed?(self, User.current, method)
-          self.send(unchecked_method, *args)
-        else
-          raise "Access Violation"
+    def guard(*methods)
+      methods.each do |m|
+        @parent.stonewall.guarded_methods << m
+        aliased_target, punctuation = m.to_s.sub(/([?!=])$/, ''), $1
+        checked_method = "#{aliased_target}_with_stonewall#{punctuation}"
+        unchecked_method = "#{aliased_target}_without_stonewall#{punctuation}"    
+        # --------------
+        # This method is defined on the guarded class, so it is callable on
+        # objects of that class.  This is 1/3rd of the magic of this gem-
+        # if you declare 'schpoo' a guarded method, we generate this
+        # 'schpoo_with_stonewall' method. Elsewhere, we use alias_method_chain
+        # to wrap your original 'schpoo' method.
+        @parent.send(:define_method, checked_method) do |*args|
+          if stonewall.allowed?(self, User.current, m)
+            self.send(unchecked_method, *args)
+          else
+            raise "Access Violation"
+          end
         end
+        # -------------- end of bizzaro meta-juju
       end
-      # -------------- end of bizzaro meta-juju
     end
 
   end
